79d862d4a3ea0c70de89f998e6438d354526d108
KomiGame-Git <komigaming97@outlook.jp>
2024-11-16 11:33:27 +0900

    RayCastTestGame更新2024.11.16

--- Assets/Scripts/RayCastBarTest.cs
+++ Assets/Scripts/RayCastBarTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RayCastBallTest : MonoBehaviour
+public class RayCastBarTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
